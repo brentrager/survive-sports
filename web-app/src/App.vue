be17df3fcee416aa971fc628533ef6be8d6c9c71
Brent Rager <brent.rager@gmail.com>
2018-10-18 00:09:57 -0400

    Got a good start on the look of the web-app.

--- web-app/src/App.vue
+++ web-app/src/App.vue
@@ -1,49 +1,10 @@
 <template>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
-            <a class="navbar-brand" href="#">Navbar</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault"
-                aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
 
-            <div class="collapse navbar-collapse" id="navbarsExampleDefault">
-                <ul class="navbar-nav mr-auto">
-                    <li class="nav-item active">
-                        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled" href="#">Disabled</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="http://example.com" id="dropdown01" data-toggle="dropdown"
-                            aria-haspopup="true" aria-expanded="false">Dropdown</a>
-                        <div class="dropdown-menu" aria-labelledby="dropdown01">
-                            <a class="dropdown-item" href="#">Action</a>
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <a class="dropdown-item" href="#">Something else here</a>
-                        </div>
-                    </li>
-                </ul>
-                <button class="btn btn-outline-success my-2 my-sm-0" v-if="!authenticated" @click="auth.login()">Sign-In</button>
-            </div>
-        </nav>
+        <nav-bar></nav-bar>
 
         <main role="main" class="container">
 
-            <div class="starter-template">
-                <h1>Survive Sports</h1>
-                <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text
-                    and a mostly barebones HTML document.</p>
-            </div>
-
-            <div id="nav">
-                <router-link to="/">Home</router-link> |
-                <router-link to="/about">About</router-link>
-            </div>
             <router-view/>
 
         </main><!-- /.container -->
@@ -52,6 +13,7 @@
 
 <script>
     import AuthService from './AuthService';
+    import NavBar from './components/NavBar';
 
     const auth = new AuthService();
     const authenticated = auth.authenticated;
@@ -67,32 +29,19 @@
                 authenticated,
             };
         },
+        components: {
+            NavBar,
+        },
     };
 </script>
 
-<style>
-    #app {
-        font-family: 'Avenir', Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        text-align: center;
-        color: #2c3e50;
-    }
-    #nav {
-        padding: 30px;
-    }
-
-    #nav a {
-        font-weight: bold;
-        color: #2c3e50;
-    }
-
-    #nav a.router-link-exact-active {
-        color: #42b983;
-    }
+<style lang="scss">
+    @import '../node_modules/bootstrap/dist/css/bootstrap.css';
 
     body {
         padding-top: 5rem;
+        background-color: $background-color;
+        color: $text-color;
     }
     .starter-template {
         padding: 3rem 1.5rem;
---
+++ web-app/src/components/NavBar.vue
@@ -0,0 +1,50 @@
+<template>
+    <nav class="navbar navbar-expand-md navbar-dark fixed-top">
+        <router-link to="/" class="navbar-brand">Survive Sports</router-link>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarsExampleDefault"
+            aria-controls="navbarsExampleDefault" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarsExampleDefault">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <router-link active-class="active" to="/" class="nav-link" exact>Home</router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link active-class="active" to="/about" class="nav-link" exact>About</router-link>
+                </li>
+            </ul>
+            <button class="btn btn-outline-success my-2 my-sm-0" v-if="!authenticated" @click="auth.login()">Sign-In</button>
+        </div>
+    </nav>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({ name: 'nav-bar'})
+export default class NavBar extends Vue {}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+nav {
+    background-color: $banner-color;
+}
+
+a.navbar-brand {
+    color: $logo-color;
+    font-weight: bolder;
+}
+
+nav button.btn-outline-success {
+    color: $banner-text-color;
+    border-color: $banner-text-color;
+}
+
+nav button.btn-outline-success:hover {
+    color: $text-color;
+    background-color: $banner-text-color;
+}
+</style>
